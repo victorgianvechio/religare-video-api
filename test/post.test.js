1009624c7ec718433787a5d707c9c5e1aa30f485
Victor Gianvechio <victor.gianvecchio@live.com>
2021-07-05 10:20:27 -0300

    post.test.js

--- test/post.test.js
+++ test/post.test.js
@@ -6,11 +6,13 @@ import request from 'supertest';
 import publicPath from '../src/utils/publicPath';
 import app from '../src/App';
 
-describe('POST /upload', () => {
+describe('POST /api/v1/upload', () => {
   test('Realiza upload de arquivos na DigitalOcean Space', async () => {
     const result = await request(app)
       .post('/api/v1/upload')
-      .attach('upload', path.resolve(publicPath, 'file-test.png'));
+      .attach('upload', path.resolve(publicPath, 'assets', 'file-test.png'));
+
+    console.log(result.body.file);
 
     expect(result.statusCode).toBe(200);
   });
