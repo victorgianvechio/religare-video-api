f541365fab8c08713c8a77649ba50afdc4df4c98
Victor Gianvechio <victor.gianvecchio@live.com>
2021-07-08 13:26:20 -0300

    refatorado

--- src/controller/UploadController.js
+++ src/controller/UploadController.js
@@ -3,7 +3,10 @@ import DigitalOceanService from '../services/DigitalOceanService';
 class UploadController {
   async uploadFile(request, response, next) {
     DigitalOceanService.upload(request, response, error => {
-      if (error) return response.status(500).json({ status: false, error });
+      if (error) {
+        console.log(error);
+        return response.status(500).json({ status: false, error });
+      }
 
       return response.status(200).json({
         status: true,
